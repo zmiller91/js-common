63866c58fbdc833bea31eb4c8b7392cfc4b81fab
unknown <2.4.8.16.32.sixtyfour@gmail.com>
2016-11-06 00:53:11 -0600

    User Login and Registration Modals
        Added an AngularJS controller to work with
        login and registration endpoints. These
        modals got a major face lift from the previous
        version. These modals are also more responsive
        to user interaction and errors returned from the
        server

---
+++ user/user.js
@@ -0,0 +1,248 @@
+define([], function() {
+    return {
+        init: function(app) {
+            app.controller('UserCtrl', function($scope, $uibModal, User) {
+
+                $scope.user = {};
+
+                $scope.register = function () {
+                    $uibModal.open({
+                        templateUrl: 'lib/js-common/user/register_modal.html',
+                        controller: 'RegistrationCtrl',
+                        size: 'sm'
+                    });
+                };    
+
+                $scope.login = function () {
+                    $uibModal.open({
+                        templateUrl: 'lib/js-common/user/login_modal.html',
+                        controller: 'LoginCtrl',
+                        size: 'sm'
+                    });
+                };
+
+                $scope.logout = function(){
+                    User.logout( 
+                        function() {}, 
+                        function() {}
+                    );
+                };
+
+                $scope.$on('user:updated', function(event,data) {
+                    $scope.user = data;
+                });
+
+                angular.element(document).ready(function () {
+                    User.authorizeCookie();
+                });
+            });
+
+            app.controller('LoginCtrl', function ($scope, $uibModalInstance, User) {
+                
+                $scope.user = {errors: []};
+                $scope.creds = {name: "", pass: ""};
+                $scope.loading = false;
+                
+                $scope.submit = function(){
+                    $scope.user.errors = [];
+                    var hasError = false;
+                    
+                    // Username is required
+                    if(!$scope.creds.name || $scope.creds.name.length === 0) {
+                        $scope.user.errors.push("Username is required.");
+                        hasError = true;
+                    }
+                    
+                    // Password is required
+                    if(!$scope.creds.pass || $scope.creds.pass.length === 0) {
+                        $scope.user.errors.push("Password is required.");
+                        hasError = true;
+                    }
+                    
+                    if(!hasError) {
+                        $scope.loading = true;
+                        User.login(
+                            $scope.creds, 
+                            function(user) {
+                                if(user.loggedIn) {
+                                    $uibModalInstance.close(user);
+                                }
+                                
+                                $scope.loading = false;
+                            }, 
+                            function(response) 
+                            {
+                                var errors = response["data"]["errors"];
+                                if(errors)
+                                {
+                                    $scope.user.errors = errors;
+                                }
+                                else
+                                {
+                                    $scope.user.errors = ["Unknown error."];
+                                }
+                                
+                                $scope.loading = false;
+                            }
+                        );
+                    }
+                };
+
+                $scope.cancel = function () {
+                    $uibModalInstance.dismiss('cancel');
+                };
+                
+                $scope.keypress = function(keyEvent)
+                {
+                    if (keyEvent.which === 13)
+                    {
+                        $scope.submit();
+                    }
+                };
+
+                $scope.$on('user:updated', function(event,data) {
+                    $scope.user = data;
+                });
+            });
+
+            app.controller('RegistrationCtrl', function ($scope, $uibModalInstance, User) {
+
+                $scope.user = {name: "", pass: "", verified_pass: "", errors: []};
+                $scope.loading = false;
+                
+                $scope.submit = function () {
+                    $scope.user.errors = [];
+                    var hasError = false;
+                    
+                    // Username is required
+                    if(!$scope.user.name || $scope.user.name.length === 0) {
+                        $scope.user.errors.push("Username is required.");
+                        hasError = true;
+                    }
+                    
+                    // Password is required
+                    if(!$scope.user.pass || $scope.user.pass.length === 0) {
+                        $scope.user.errors.push("Password is required.");
+                        hasError = true;
+                    }
+                    
+                    // Verification password is required
+                    if(!$scope.user.verified_pass || $scope.user.verified_pass.length === 0) {
+                        $scope.user.errors.push("Verification password is required.");
+                        hasError = true;
+                    }
+                    
+                    // Passwords must match
+                    if ($scope.user.pass !== $scope.user.verified_pass) {
+                        $scope.user.errors.push("Password missmatch.");
+                        hasError = true;
+                    }
+                    
+                    if (!hasError) {
+                        $scope.loading = true;
+                        User.register(
+                            $scope.user, 
+                            function(user) 
+                            {
+                                if(user.loggedIn) {
+                                    $scope.user = {};
+                                    $uibModalInstance.close(user);
+                                }
+                                
+                                $scope.loading = false;
+                            }, 
+                            function(response) 
+                            {
+                                var errors = response["data"]["errors"];
+                                if(errors)
+                                {
+                                    $scope.user.errors = errors;
+                                }
+                                else
+                                {
+                                    
+                                $scope.user.errors = ["Unknown error."];
+                                }
+                                
+                                $scope.loading = false;
+                            }
+                        );
+                    } 
+                };
+
+                $scope.cancel = function () {
+                    $uibModalInstance.dismiss('cancel');
+                };
+                
+                $scope.keypress = function(keyEvent)
+                {
+                    if (keyEvent.which === 13)
+                    {
+                        $scope.submit();
+                    }
+                };
+
+                $scope.$on('user:updated', function(event, data) {
+                    $scope.user = angular.extend($scope.user, data);
+                });
+            });
+
+            app.service('User', ['$rootScope', '$http', function($rootScope, $http) {
+
+                this.response;
+                this.data;
+                this.loggedIn = false;
+                this.id;
+                this.name;
+                this.errors;
+                
+                this.login = function(creds, success, error) {
+                    var data = {user: creds, method: 'login'};
+                    $http.post('/api/login', data)
+                    .then(function(response) {
+                        parseResponse(response);
+                        success(response.data);
+                    }, function(response) {
+                        error(response);
+                    });
+                };
+                
+                this.logout = function(success, error) {
+                    var oData = {user: this.id, method: 'logout'};
+                    $http.post('/api/logout', oData)
+                        .then(function(response) {
+                            parseResponse(response);
+                            success(response.data);
+                        }, function(response) {
+                            error(response);
+                    });
+                }
+                
+                this.register = function(user, success, error) {
+                    var oData = {user: user};
+                    $http.post('/api/register', oData)
+                        .then(function(response) {
+                            parseResponse(response);
+                            success(response.data);
+                        }, function(response) {
+                            error(response);
+                    });
+                }
+
+                this.authorizeCookie = function() {
+                    $http.post('/api/authorize')
+                    .then(parseResponse, function() {});
+                };
+                
+                function parseResponse(response) {
+                    this.data = response.data;
+                    this.id = this.data.id;
+                    this.name = this.data.name;
+                    this.loggedIn = this.data.loggedIn;
+                    this.errors = this.data.errors;
+                    $rootScope.$broadcast('user:updated',this.data);
+                }
+            }]);
+        }
+    };
+});
